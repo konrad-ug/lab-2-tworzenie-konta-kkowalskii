69f6281ea0cb449d253d518ca4add08535ed8835
kkowalskii <92157049+kkowalskii@users.noreply.github.com>
2022-10-26 21:05:44 +0200

    Lab2/koniec zadania

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,12 @@
+import re
+
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        self.pesel = pesel
+        self.kod_rabatowy = kod_rabatowy
+        if (self.kod_rabatowy) and (self.pesel): 
+            if (bool(re.search('^PROM_...$', kod_rabatowy))) == True:
+                self.saldo = 50
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -1,13 +1,36 @@
+from types import NoneType
 import unittest
+import re
 
 from ..Konto import Konto
 
-class TestCreateBankAccount(unittest.TestCase):
 
+
+class TestCreateBankAccount(unittest.TestCase):
+    
+    
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "05526589435", "PROM_xyz")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, "05526589435", "Pesel nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "Niepoprawny pesel!")
+        if (pierwsze_konto.kod_rabatowy) == NoneType:
+            self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        else:
+            self.assertTrue((re.search('^PROM_...$', pierwsze_konto.kod_rabatowy)), "Niepoprawny kod rabatowy!")
+
+        self.assertEqual(((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
+        
+                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12
+                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 50) or not ((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
+
+                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12
+                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 0), True, "Rok urodzenia musi być większy niż 1960!")
+        
+        
+        
+
+
 
     #tutaj proszę dodawać nowe testy
