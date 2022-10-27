3ea5de5360b8b518a5d9784012a7d89990f3af61
Kosma Kowalski <kkowalski@INF.UG.EDU.PL>
2022-10-27 17:39:42 +0200

    poprawa Lab2

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,12 @@
 import re
 
+def between_two_numbers(min, max, number):
+    if number >= min and number <= max:
+        return True
+    else:
+        return False
+
+
 class Konto:
     def __init__(self, imie, nazwisko, pesel, kod_rabatowy=None):
         self.imie = imie
@@ -7,6 +14,23 @@ class Konto:
         self.saldo = 0
         self.pesel = pesel
         self.kod_rabatowy = kod_rabatowy
-        if (self.kod_rabatowy) and (self.pesel): 
+        if (self.kod_rabatowy) and (self.pesel):
             if (bool(re.search('^PROM_...$', kod_rabatowy))) == True:
+                if between_two_numbers(13, 80, int(self.pesel[2:4])) or (int(self.pesel[0:2]) >= 61 and between_two_numbers(1, 12, int(self.pesel[2:4]))):
+                    self.saldo = 50
+                elif between_two_numbers(1, 12, int(self.pesel[2:4]))
+
+
+            
                 self.saldo = 50
+        
+
+        (((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
+
+                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12 ### wlasnie tutaj skonczylem
+                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 50) or not ((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
+
+                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12
+                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 0), True, "Rok urodzenia musi być większy niż 1960!")
+        
+
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -7,10 +7,14 @@ from ..Konto import Konto
 
 
 class TestCreateBankAccount(unittest.TestCase):
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    pesel = "32134529423"
+    kod_rabatowy = "PROM_xyz"
     
     
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski", "05526589435", "PROM_xyz")
+        pierwsze_konto = Konto(self.imie, self.nazwisko, self.pesel, eslf.)
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.pesel, "05526589435", "Pesel nie został zapisany!")
@@ -20,14 +24,8 @@ class TestCreateBankAccount(unittest.TestCase):
         else:
             self.assertTrue((re.search('^PROM_...$', pierwsze_konto.kod_rabatowy)), "Niepoprawny kod rabatowy!")
 
-        self.assertEqual(((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
-
-                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12
-                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 50) or not ((int(pierwsze_konto.pesel[2:4]) > 12 and int(pierwsze_konto.pesel[2:4]) < 81)
-
-                        or (int(pierwsze_konto.pesel[0:2]) >= 61 and int(pierwsze_konto.pesel[2:4]) <= 12
-                            and int(pierwsze_konto.pesel[2:4]) > 0)) and (pierwsze_konto.saldo == 0), True, "Rok urodzenia musi być większy niż 1960!")
-    #przepraszam za commit na main branch, zagapiłem się 
+        
+     
 
     #tutaj proszę dodawać nowe testy
 
